42277b824970709c70df38da586effb089e0a873
Sunrise Script <ambre+bot@tarides.com>
2025-12-05 01:10:35 +0100

    Added template for day 3

--- utils/all.ml
+++ utils/all.ml
@@ -5,6 +5,6 @@ module type DAY = sig
 end
 
 let days : (module DAY) array =
-  [| (module Day0); (module Day1); (module Day2) |]
+  [| (module Day0); (module Day1); (module Day2); (module Day3) |]
 
-let expected = [ ""; "6"; "4174379265" ]
+let expected = [ ""; "6"; "4174379265"; "357" ]
