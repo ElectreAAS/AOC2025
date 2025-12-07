6a6e12564d8707b01ce82af5f4ec3469c0c10554
Ambre Austen Suhamy <ambre@tarides.com>
2025-12-07 22:36:09 +0100

    Solved day 7 part 2

--- day7/day7.ml
+++ day7/day7.ml
@@ -1,8 +1,8 @@
-type cell = Empty | Beam | Splitter
+type cell = Empty | Beam of int | Splitter
 
 let parse_cell = function
   | '.' -> Empty
-  | '|' -> Beam
+  | '|' -> Beam 1
   | '^' -> Splitter
   | c -> invalid_arg (Printf.sprintf "%c" c)
 
@@ -12,23 +12,25 @@ let parse_line line =
 let parse_first_line line = String.index line 'S'
 
 let step row grid =
-  let _, sum =
-    Array.fold_left
-      (fun (i, acc) -> function
-        | Empty | Splitter -> (i + 1, acc)
-        | Beam -> (
-            match grid.(row + 1).(i) with
-            | Empty ->
-                grid.(row + 1).(i) <- Beam;
-                (i + 1, acc)
-            | Beam -> (i + 1, acc)
-            | Splitter ->
-                grid.(row + 1).(i - 1) <- Beam;
-                grid.(row + 1).(i + 1) <- Beam;
-                (i + 1, acc + 1)))
-      (0, 0) grid.(row)
-  in
-  sum
+  Array.iteri
+    (fun i -> function
+      | Empty | Splitter -> ()
+      | Beam n -> (
+          match grid.(row + 1).(i) with
+          | Empty -> grid.(row + 1).(i) <- Beam n
+          | Beam m -> grid.(row + 1).(i) <- Beam (m + n)
+          | Splitter -> (
+              (* LEFT *)
+              (match grid.(row + 1).(i - 1) with
+              | Empty -> grid.(row + 1).(i - 1) <- Beam n
+              | Beam m -> grid.(row + 1).(i - 1) <- Beam (m + n)
+              | Splitter -> failwith "Two splitters side by side?");
+              (* RIGHT *)
+              match grid.(row + 1).(i + 1) with
+              | Empty -> grid.(row + 1).(i + 1) <- Beam n
+              | Beam m -> grid.(row + 1).(i + 1) <- Beam (m + n)
+              | Splitter -> failwith "Two splitters side by side?")))
+    grid.(row)
 
 let day _display _pool input_buffer =
   let first_line = Eio.Buf_read.line input_buffer in
@@ -36,11 +38,17 @@ let day _display _pool input_buffer =
   let lines = Eio.Buf_read.lines input_buffer in
   let grid = Seq.map parse_line lines |> Array.of_seq in
   let height = Array.length grid in
-  grid.(0).(position) <- Beam;
-  let rec loop i sum =
-    if i = height - 1 then sum
-    else
-      let nb_splits = step i grid in
-      loop (i + 1) (sum + nb_splits)
+  grid.(0).(position) <- Beam 1;
+  let rec loop i =
+    if i < height - 1 then (
+      step i grid;
+      loop (i + 1))
   in
-  loop 0 0
+  loop 0;
+  Array.fold_left
+    (fun sum -> function
+      | Empty -> sum
+      | Beam n -> sum + n
+      | Splitter -> failwith "Splitters in the last row?")
+    0
+    grid.(height - 1)
