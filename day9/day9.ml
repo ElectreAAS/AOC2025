8362fe5b64d65ccc248d1d25fdc0a31ff9871915
Ambre Austen Suhamy <ambre@tarides.com>
2025-12-09 14:33:13 +0100

    Solved day 9 part 1

--- day9/day9.ml
+++ day9/day9.ml
@@ -1,3 +1,19 @@
 let day _display _pool input_buffer =
-  let line = Eio.Buf_read.line input_buffer in
-  String.length line
+  let lines = Eio.Buf_read.lines input_buffer in
+  let tiles =
+    Array.of_seq
+      (Seq.map
+         (fun line -> Scanf.sscanf line "%d,%d" (fun x y -> (x, y)))
+         lines)
+  in
+  let size = Array.length tiles in
+  let rec loop i j max_area =
+    if i = size - 1 then max_area
+    else if j = size then loop (i + 1) (i + 2) max_area
+    else
+      let ix, iy = tiles.(i) and jx, jy = tiles.(j) in
+      let area = (abs (ix - jx) + 1) * (abs (iy - jy) + 1) in
+      loop i (j + 1) (max max_area area)
+  in
+
+  loop 0 1 0
